662e46a69e65b82e29469b939ecee8612d9f0045
Laura Molkas <100437029+Lauraa0001@users.noreply.github.com>
2022-03-26 19:42:59 +0100

    AnimeFN leak Backend
    Put Backend In

---
+++ animefndev12/config.js
@@ -0,0 +1,29 @@
+module.exports = {
+    host: 'localhost',
+    port: 5595,
+    directory: __dirname,
+    dir: __dirname,
+    season_num: 20, // Update this whenever there is a new season.
+    banner: "https://eurovision-api.qtfraz.repl.co/animefnbanner.png",
+    background: 'winter2021'
+    /*
+    defaultnotris
+    worldcup - WorldCup
+    winter19 - fortnitemares
+    GalileoLite - Star Wars Lite
+    halloween2020 - Halloween 
+    season15xmas - Cool XMas Background
+    fortnitemares - Somehow Winterfest 2019 
+    BlackMonday - Batman 
+    Galileo - Star Wars
+    season17 - Mother Season
+    season16 - Primal Season
+    season15 - Shit season
+    season15xmas - Winterfest 2020
+    season14 - Marvel
+    season13 - Water season
+    season12 - Spy season 
+    season11 - Chapter 2
+    seasonx - Season 10
+    */
+}
